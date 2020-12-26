595477fe915ce77c670f76fcba6080678d89fa6d
Olga Letkova <olja.let52@gmail.com>
2020-12-26 18:52:51 +0300

    Creation of test for data's checking

---
+++ src/test/java/ru/netology/rest/PostmanApiTest.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+class PostmanApiTest {
+@Test
+void shouldCheckRequestPost(){
+    given()
+            .baseUri("https://postman-echo.com")
+            .body("20.06.2020")
+            .when()
+            .post("/post")
+            .then()
+            .statusCode(200)
+            .body("data", equalTo("20.06.2020"));
+}
+}
